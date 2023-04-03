c520c1e0b55be234c4b54bbf3f61dd539853a0c5
stanno03 <danno_41@hotmail.com>
2023-04-04 00:51:00 +1000

    completed specialChars array

--- Develop/script.js
+++ Develop/script.js
@@ -2,7 +2,7 @@
 let lowerKeys =['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 let upperKeys = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J',' K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
 let numericValues = ['1','2','3','4','5','6','7','8','9','0'];
-let specialChars = ["!", "@", "#" , "$", "%","^", "&", "*", "(", ")", "-","+"];
+let specialChars = ["!", "@", "#" , "$", "%","^", "&", "*", "(", ")", "-","+", "", ",", ".", "/", ":",";","<","=",">","?","@", "[", "]","_", "`","{","}","|", "~"];
 
 var generateBtn = document.querySelector("#generate");
 // function that runs when generate password button is clicked and prompts for inputs to generate then display the password
